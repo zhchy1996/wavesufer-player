31eeca7520f22dd3f6aeb56cc92bb87208bc0935
chengyuzhang <chengyuzhang@yangqianguan.com>
2020-08-26 16:47:14 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'audio-player'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
